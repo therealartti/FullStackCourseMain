ea6798f9bcc2ca7381664b2b631c255a243ec7c2
therealartti <107062680+therealartti@users.noreply.github.com>
2023-06-14 22:11:04 +0300

    Add files via upload

---
+++ part4/4.1-4.7/utils/list_helper.js
@@ -0,0 +1,61 @@
+const logger = require('../utils/logger')
+const _ = require('lodash')
+
+
+const dummy = (blogs) => {
+    return(1)
+}
+
+const totalLikes  = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog  = (blogs) => {
+    const reducer = (fav, item) => {
+        if (fav.likes > item.likes){
+            return {
+                title: fav.title,
+                author: fav.author,
+                likes: fav.likes}
+        }
+        return {
+            title: item.title,
+            author: item.author,
+            likes: item.likes}
+    }
+    return blogs.reduce(reducer, {})
+}
+
+const mostBlogs = (blogs) => {
+    if (!blogs.length) {
+        return {}
+    }
+    const authors = _(blogs).groupBy('author').value()
+    logger.info(authors)
+    logger.info(Object.keys(authors).length)
+
+    const mostCommonAuthor = _.head(_(blogs)
+                    .countBy('author')
+                    .entries()
+                    .maxBy(_.last))
+    logger.info("Most common " + mostCommonAuthor)
+    const mostCommonAuthorCount = authors[mostCommonAuthor].length
+    logger.info("His count: " + mostCommonAuthorCount)
+    return {author: mostCommonAuthor, blogs: mostCommonAuthorCount }
+}
+
+const mostLikes = (blogs) => {
+    logger.info(blogs)
+    if (!blogs.length) {
+        return {}
+    }
+    const maxLikes = blogs.reduce((max, blog) => max.likes >= blog.likes ? max : blog)
+    return {author : maxLikes.author, likes: maxLikes.likes}
+}
+  
+module.exports = {
+    dummy , totalLikes , favoriteBlog, mostBlogs , mostLikes 
+}
